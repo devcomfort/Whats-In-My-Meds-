b843c8b722bc14e027a57cc565e795084b5a7a16
devcomfort <kimdonghyun026@gmail.com>
2023-12-06 09:56:54 +0900

    wip : CRUD 테스트 중 (Drizzle 적응 목적)

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,29 @@
 import { Hono } from 'hono'
 
-const app = new Hono()
+import { users } from './model/user';
+import { drizzle } from 'drizzle-orm/d1';
 
-app.get('/', (c) => c.text('Hello Hono!'))
+const app = new Hono<{
+  Bindings: {
+    DB: D1Database
+  }
+}>()
+
+app.get('/build', async (c) => {
+  const db = drizzle(c.env.DB);
+  const result = await db.insert(users).values({
+    id: "1",
+    intModifiers: true,
+    textModifiers: "100"
+  })
+
+  return c.json(result.results)
+})
+
+app.get('/', async (c) => {
+  const db = drizzle(c.env.DB);
+  const result = await db.select().from(users).all()
+  return c.json(result)
+})
 
 export default app
